076544e1221124a45316ca6325ec4a96c00729f4
Melvin0406 <124396019+Melvin0406@users.noreply.github.com>
2025-05-12 20:35:45 -0700

    Add files via upload

---
+++ ServicioTocho/ServicioTocho/ServicioTochoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServicioTochoApp.swift
+//  ServicioTocho
+//
+//  Created by CETYS Universidad  on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ServicioTochoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ServicioTocho/ServicioTochoTests/ServicioTochoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ServicioTochoTests.swift
+//  ServicioTochoTests
+//
+//  Created by CETYS Universidad  on 12/05/25.
+//
+
+import Testing
+@testable import ServicioTocho
+
+struct ServicioTochoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
